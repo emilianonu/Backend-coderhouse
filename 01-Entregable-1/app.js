19c3d46dbb97a75bfc102c2256c85fdf0d25c08f
Emiliano <emilianonu@gmail.com>
2022-07-13 20:23:35 -0300

    Primer desafio entregable

---
+++ 01-Entregable-1/app.js
@@ -0,0 +1,34 @@
+class Usuario {
+    contructor(nombre, apellido, libros=[], mascotas=[]){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+    }
+
+    getFullName() {
+        return `${this.nombre} ${this.apellido}`;
+    }
+
+    addMascota(mascota) {
+        this.mascotas.push(mascota);
+    }
+
+    countMascotas() {
+        return `${this.mascotas.length}`;
+    }
+
+    addBook(libro) {
+        this.libros.push(libro);
+    }
+
+    getBookNames() {
+        return this.libros.map(libro => `${libro.nombre}`);
+    }
+}
+
+const usuario = new Usuario("Nombre", "Apellido");
+
+usuario.addBook([{libro: "Libro", autor: "Autor"}]);
+
+console.log(usuario.getBookNames());
